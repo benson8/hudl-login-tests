425c30033897a817177c98a3a06442b9ef6246f5
Ben Rogers <benson8@gmail.com>
2023-04-29 15:12:40 -0500

    Formatting improvements for Eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,20 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "mocha": true,
-        "es2021": true
+  env: {
+    node: true,
+    commonjs: true,
+    mocha: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    react: {
+      version: "18.16.0",
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    },
-    "settings": {
-        "react": {
-            "version": "18.16.0"
-        } 
-    }
-}
+  },
+};
