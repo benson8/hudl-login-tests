1498742ffebfe3da2f511ff63cb5b06bfd765f1c
Ben Rogers <benson8@gmail.com>
2023-04-30 11:39:01 -0500

    Exclude Safari runs from test-runner.sh on Windows

--- test-runner.sh
+++ test-runner.sh
@@ -9,8 +9,12 @@ env BROWSER="chrome" MOBILE_WEB="false" HEADLESS="false" npx mocha tests
 echo "Chrome Mobile - Pixel 5"
 env BROWSER="chrome" MOBILE_WEB="true" HEADLESS="false" npx mocha tests
 
-echo "Safari Desktop"
-env BROWSER="safari" MOBILE_WEB="false" HEADLESS="false" npx mocha tests
+# Skip Safari on Windows since it's no longer supported
+if [ "$OS" != "Windows_NT" ]
+then
+  echo "Safari Desktop"
+  env BROWSER="safari" MOBILE_WEB="false" HEADLESS="false" npx mocha tests
+fi
 
 echo "Chrome Desktop - Headless"
 env BROWSER="chrome" MOBILE_WEB="false" HEADLESS="true" npx mocha tests
